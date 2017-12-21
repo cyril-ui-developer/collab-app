6dc3218ca46f321be4c37b14f3ce89e2b16fa5d3
Cyril Ajieh <cyril.ajieh@cyrils-mbp.attlocal.net>
2017-12-21 05:48:12 -0600

    installed bootstrap 4 and configured sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule
